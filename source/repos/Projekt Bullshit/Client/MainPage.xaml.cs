eae2b14d4031a990a19343ba87f9c1bc445b9920
Fota <fotasteam@gmail.com>
2022-05-08 19:56:28 +0200

    update

---
+++ source/repos/Projekt Bullshit/Client/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net;
+using System.Net.Sockets;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
+
+namespace Client
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {            
+            Int32 port = 13000;
+            TcpClient client = new TcpClient("127.0.0.1", port);
+
+            // Translate the passed message into ASCII and store it as a Byte array.
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(textBox1.Text);
+
+            // Get a client stream for reading and writing.
+            //  Stream stream = client.GetStream();
+
+            NetworkStream stream = client.GetStream();
+
+            // Send the message to the connected TcpServer.
+            stream.Write(data, 0, data.Length);
+
+            Console.WriteLine("Sent: {0}", textBox1.Text);
+
+            // Receive the TcpServer.response.
+
+            // Buffer to store the response bytes.
+            data = new Byte[256];
+
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            Console.WriteLine("Received: {0}", responseData);
+
+            // Close everything.
+            stream.Close();
+            client.Close();
+        }
+       
+    }
+}
---
+++ source/repos/Projekt Bullshit/Host/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("[BS] - Host initialized"); //stworz port i adres
+
+Int32 port = 13000;
+IPAddress localadress = IPAddress.Parse("127.0.0.1");
+
+Console.WriteLine("[BS] - Starting server"); //uruchom serwer
+
+TcpListener server = new TcpListener(localadress, port);
+
+server.Start();
+
+Byte[] bytes = new Byte[256];
+String data = null;
+
+Console.WriteLine("[BS] - Started listening");
+
+while (true)
+{
+    Console.Write("[BS] - Awaiting connection... ");
+    TcpClient client = server.AcceptTcpClient();
+    Console.WriteLine("Connected!");
+
+    data = null;
+
+    NetworkStream stream = client.GetStream();
+
+    int i;
+
+    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+    {
+        // Translate data bytes to a ASCII string.
+        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+        Console.WriteLine("Received: {0}", data);
+
+        // Process the data sent by the client.
+        data = data.ToUpper();
+
+        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+        // Send back a response.
+        stream.Write(msg, 0, msg.Length);
+        Console.WriteLine("Sent: {0}", data);
+    }
+
+    client.Close();
+}
+
+server.Stop();
+
+Console.WriteLine("[BS] - Initiated shutdown");
+Console.WriteLine("[BS] - Client closed");
+Console.WriteLine("[BS] - Stopped listening");
+Console.WriteLine("[BS] - Server closed");
+
+Console.Read();
